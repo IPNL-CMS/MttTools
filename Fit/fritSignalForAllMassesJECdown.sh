57070730639039ba67d280e64327c9880f055725
Sébastien Brochet <s.brochet@ipnl.in2p3.fr>
2013-01-10 10:27:18 +0100

    [Fit] added helper scripts to run fit on signal

---
+++ Fit/fritSignalForAllMassesJECdown.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+for mass in 750 1000 1250 1500; do
+  echo "Fit signal for M=$mass"
+  ./fritSignal -m $mass --b-tag 2 -i ../Extractor2Dataset/MTT_Zprime_${mass}_Narrow_*_merged_*_JECdown.root --jec JECdown
+  ./fritSignal -m $mass --b-tag 1 -i ../Extractor2Dataset/MTT_Zprime_${mass}_Narrow_*_merged_*_JECdown.root --jec JECdown
+done;
