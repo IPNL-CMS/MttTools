de2ceb55f6a73048f2aba1c29c123d9014b2ecc2
Stephane Perries <s.perries@ipnl.in2p3.fr>
2013-01-30 13:00:52 +0100

    update fot powheg

--- Extractor2Histo/process_all.sh
+++ Extractor2Histo/process_all.sh
@@ -1,3 +1,8 @@
-./process_all --data
-./process_all --semimu
-./process_all --semie
+./process_all --data --b-tag 2
+./process_all --semimu --b-tag 2
+./process_all --semie --b-tag 2
+mv *Histo*.root 2btag
+./process_all --data --b-tag 1
+./process_all --semimu --b-tag 1
+./process_all --semie --b-tag 1
+mv *Histo*.root 1btag
