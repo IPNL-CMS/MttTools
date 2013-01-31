9b2e7f60617d550d28ee627cea500f2a35b3925f
Sébastien Brochet <s.brochet@ipnl.in2p3.fr>
2013-01-31 17:01:34 +0100

    added: large Z' samples

--- Extractor2Dataset/extractMCSystematics.py
+++ Extractor2Dataset/extractMCSystematics.py
@@ -5,17 +5,29 @@ import os, subprocess, tempfile, datetime
 d = datetime.datetime.now().strftime("%d%b%y")
 
 files = [
-    ["MTT_Signal_Zprime_500_Narrow_2012_dataset_%s_JECup.root",  "Systematics/JECup/MTT_Zprime_500_Narrow_19Dec12_%s.list"],
-    ["MTT_Signal_Zprime_750_Narrow_2012_dataset_%s_JECup.root",  "Systematics/JECup/MTT_Zprime_750_Narrow_full_stat_%s.list"],
-    ["MTT_Signal_Zprime_1000_Narrow_2012_dataset_%s_JECup.root", "Systematics/JECup/MTT_Zprime_1000_Narrow_full_stat_%s.list"],
-    ["MTT_Signal_Zprime_1250_Narrow_2012_dataset_%s_JECup.root", "Systematics/JECup/MTT_Zprime_1250_Narrow_full_stat_%s.list"],
-    ["MTT_Signal_Zprime_1500_Narrow_2012_dataset_%s_JECup.root", "Systematics/JECup/MTT_Zprime_1500_Narrow_full_stat_%s.list"],
+    #["MTT_Signal_Zprime_500_Narrow_2012_dataset_%s_JECup.root",  "Systematics/JECup/MTT_Zprime_500_Narrow_full_stat_%s.list"],
+    #["MTT_Signal_Zprime_750_Narrow_2012_dataset_%s_JECup.root",  "Systematics/JECup/MTT_Zprime_750_Narrow_full_stat_%s.list"],
+    #["MTT_Signal_Zprime_1000_Narrow_2012_dataset_%s_JECup.root", "Systematics/JECup/MTT_Zprime_1000_Narrow_full_stat_%s.list"],
+    #["MTT_Signal_Zprime_1250_Narrow_2012_dataset_%s_JECup.root", "Systematics/JECup/MTT_Zprime_1250_Narrow_full_stat_%s.list"],
+    #["MTT_Signal_Zprime_1500_Narrow_2012_dataset_%s_JECup.root", "Systematics/JECup/MTT_Zprime_1500_Narrow_full_stat_%s.list"],
 
-    ["MTT_Signal_Zprime_500_Narrow_2012_dataset_%s_JECdown.root",  "Systematics/JECdown/MTT_Zprime_500_Narrow_19Dec12_%s.list"],
-    ["MTT_Signal_Zprime_750_Narrow_2012_dataset_%s_JECdown.root",  "Systematics/JECdown/MTT_Zprime_750_Narrow_full_stat_%s.list"],
-    ["MTT_Signal_Zprime_1000_Narrow_2012_dataset_%s_JECdown.root", "Systematics/JECdown/MTT_Zprime_1000_Narrow_full_stat_%s.list"],
-    ["MTT_Signal_Zprime_1250_Narrow_2012_dataset_%s_JECdown.root", "Systematics/JECdown/MTT_Zprime_1250_Narrow_full_stat_%s.list"],
-    ["MTT_Signal_Zprime_1500_Narrow_2012_dataset_%s_JECdown.root", "Systematics/JECdown/MTT_Zprime_1500_Narrow_full_stat_%s.list"]
+    #["MTT_Signal_Zprime_500_Narrow_2012_dataset_%s_JECdown.root",  "Systematics/JECdown/MTT_Zprime_500_Narrow_full_stat_%s.list"],
+    #["MTT_Signal_Zprime_750_Narrow_2012_dataset_%s_JECdown.root",  "Systematics/JECdown/MTT_Zprime_750_Narrow_full_stat_%s.list"],
+    #["MTT_Signal_Zprime_1000_Narrow_2012_dataset_%s_JECdown.root", "Systematics/JECdown/MTT_Zprime_1000_Narrow_full_stat_%s.list"],
+    #["MTT_Signal_Zprime_1250_Narrow_2012_dataset_%s_JECdown.root", "Systematics/JECdown/MTT_Zprime_1250_Narrow_full_stat_%s.list"],
+    #["MTT_Signal_Zprime_1500_Narrow_2012_dataset_%s_JECdown.root", "Systematics/JECdown/MTT_Zprime_1500_Narrow_full_stat_%s.list"]
+
+    ["MTT_Signal_Zprime_500_Large_2012_dataset_%s_JECup.root",  "Systematics/JECup/MTT_Zprime_500_Large_full_stat_%s.list"],
+    ["MTT_Signal_Zprime_750_Large_2012_dataset_%s_JECup.root",  "Systematics/JECup/MTT_Zprime_750_Large_full_stat_%s.list"],
+    ["MTT_Signal_Zprime_1000_Large_2012_dataset_%s_JECup.root", "Systematics/JECup/MTT_Zprime_1000_Large_full_stat_%s.list"],
+    ["MTT_Signal_Zprime_1250_Large_2012_dataset_%s_JECup.root", "Systematics/JECup/MTT_Zprime_1250_Large_full_stat_%s.list"],
+    ["MTT_Signal_Zprime_1500_Large_2012_dataset_%s_JECup.root", "Systematics/JECup/MTT_Zprime_1500_Large_full_stat_%s.list"],
+
+    ["MTT_Signal_Zprime_500_Large_2012_dataset_%s_JECdown.root",  "Systematics/JECdown/MTT_Zprime_500_Large_full_stat_%s.list"],
+    ["MTT_Signal_Zprime_750_Large_2012_dataset_%s_JECdown.root",  "Systematics/JECdown/MTT_Zprime_750_Large_full_stat_%s.list"],
+    ["MTT_Signal_Zprime_1000_Large_2012_dataset_%s_JECdown.root", "Systematics/JECdown/MTT_Zprime_1000_Large_full_stat_%s.list"],
+    ["MTT_Signal_Zprime_1250_Large_2012_dataset_%s_JECdown.root", "Systematics/JECdown/MTT_Zprime_1250_Large_full_stat_%s.list"],
+    ["MTT_Signal_Zprime_1500_Large_2012_dataset_%s_JECdown.root", "Systematics/JECdown/MTT_Zprime_1500_Large_full_stat_%s.list"]
   ]
 
 def launch(input, output):
