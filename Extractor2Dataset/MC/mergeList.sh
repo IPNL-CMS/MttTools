de9a2c79dfc3a3143bf09c7e86672a510df03ede
Sébastien Brochet <s.brochet@ipnl.in2p3.fr>
2013-02-05 13:25:40 +0100

    added: support for wide Z'

--- Extractor2Dataset/MC/mergeList.sh
+++ Extractor2Dataset/MC/mergeList.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-for mass in 750 1000 1250 1500 2000; do
+for mass in 500 750 1000 1250 1500 2000; do
   cat MTT_Zprime_${mass}_Narrow_*_semimu.list > MTT_Zprime_${mass}_Narrow_full_stat_semimu.list
   cat MTT_Zprime_${mass}_Narrow_*_semie.list > MTT_Zprime_${mass}_Narrow_full_stat_semie.list
+  cat MTT_Zprime_${mass}_Large_*_semimu.list > MTT_Zprime_${mass}_Large_full_stat_semimu.list
+  cat MTT_Zprime_${mass}_Large_*_semie.list > MTT_Zprime_${mass}_Large_full_stat_semie.list
 done;
