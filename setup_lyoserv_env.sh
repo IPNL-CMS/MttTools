ec06e32ccb5e0c0ac2656aa9d5eb559f46b5b4a6
Sébastien Brochet <s.brochet@ipnl.in2p3.fr>
2013-09-27 20:17:32 +0200

    Update GCC on lyoserv

--- setup_lyoserv_env.sh
+++ setup_lyoserv_env.sh
@@ -3,10 +3,7 @@
 echo "Sourcing root"
 source /gridsoft/ipnls/env/root_new64.sh
 
-echo "Sourcing gcc / gdb"
-source /gridsoft/cmss/slc5_amd64_gcc462/external/gcc/4.6.2/etc/profile.d/init.sh;
-source /gridsoft/cmss/slc5_amd64_gcc462/external/gdb/7.3.1/etc/profile.d/init.sh;
-source /gridsoft/cmss/slc5_amd64_gcc462/external/python/2.6.4/etc/profile.d/init.sh
-
-echo "Sourcing python"
-export PATH=/gridsoft/ipnls/python/Python-2.7.2/:$PATH
+echo "Sourcing gcc / gdb / python"
+source /gridsoft/cmss/slc5_amd64_gcc472/external/gcc/4.7.2/etc/profile.d/init.sh
+source /gridsoft/cmss/slc5_amd64_gcc472/external/gdb/7.6-cms/etc/profile.d/init.sh
+source /gridsoft/cmss/slc5_amd64_gcc472/external/python/2.7.3-cms6/etc/profile.d/init.sh
