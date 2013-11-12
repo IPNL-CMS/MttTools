a2b9d227b4ecd45a73460babc7ec682d04ac3687
Sébastien Brochet <s.brochet@ipnl.in2p3.fr>
2013-11-12 18:12:43 +0100

    added TCLAP in external libs

--- external/build-external.sh
+++ external/build-external.sh
@@ -16,3 +16,16 @@ make install
 
 cd ../..
 rm yaml-cpp-0.5.1.tar.gz
+
+# TCLAP
+wget http://optimate.dl.sourceforge.net/project/tclap/tclap-1.2.1.tar.gz
+tar xf tclap-1.2.1.tar.gz
+
+cd tclap-1.2.1
+./configure --prefix=$PWD/../
+
+make -j4
+make install
+
+cd ..
+rm tclap-1.2.1.tar.gz
