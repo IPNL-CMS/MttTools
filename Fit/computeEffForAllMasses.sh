2abc90a4902aa30a5561b1d6366f29919a2cce8f
Sébastien Brochet <s.brochet@ipnl.in2p3.fr>
2014-01-07 10:05:26 +0100

    [Fit] remove no longer used files

--- Fit/computeEffForAllMasses.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-for syst in "" "--jec up" "--jec down" "--jer up" "--jer down" "--pu up" "--pu down" "--pdf up" "--pdf down"; do
-  ./computeEff --b-tag 2 $syst
-  ./computeEff --b-tag 1 $syst
-done;
--- Fit/runLikelihoodScanForAllBTag.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-./runLikelihoodScan --b-tag 0
-./runLikelihoodScan --b-tag 1
-./runLikelihoodScan --b-tag 2
