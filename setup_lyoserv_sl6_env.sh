eb6c10ed722acc1d3ca544855a9ce2d6784d05ba
Sébastien Brochet <s.brochet@ipnl.in2p3.fr>
2014-01-30 09:47:12 +0100

    Update SL6 init script

--- setup_lyoserv_sl6_env.sh
+++ setup_lyoserv_sl6_env.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
 echo "Sourcing root"
-source /cvmfs/cms.cern.ch/slc6_amd64_gcc472/lcg/root/5.34.07-cms/bin/thisroot.sh
+source /gridgroup/cms/brochet/root/root-sl6/bin/thisroot.sh
 
 echo "Sourcing gcc / gdb / python / boost"
-source /cvmfs/cms.cern.ch/slc6_amd64_gcc472/external/xrootd/3.2.4-cms4/etc/profile.d/init.sh
 source /cvmfs/cms.cern.ch/slc6_amd64_gcc472/external/gcc/4.7.2-cms/etc/profile.d/init.sh
 source /cvmfs/cms.cern.ch/slc6_amd64_gcc472/external/gdb/7.6-cms/etc/profile.d/init.sh
 source /cvmfs/cms.cern.ch/slc6_amd64_gcc472/external/python/2.7.3-cms5/etc/profile.d/init.sh
