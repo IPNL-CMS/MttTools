607ab5b052317d7965522bfb7768ad0350612fbe
Anne-Laure Pequegnot <al.pequegnot@ipnl.in2p3.fr>
2014-02-14 11:14:15 +0100

    create a script specific to zprime to compute efficiencies

---
+++ Fit/computeZprimeNarrowEfficiencyForAllMasses.py
@@ -0,0 +1,16 @@
+#!/bin/env python
+
+import subprocess
+
+masses = [500, 750, 1000, 1250, 1500, 2000]
+btag = [1, 2]
+
+for mass in masses:
+    for b in btag:
+         args = ["./computeEfficiencies", "-m", str(mass), "--b-tag", str(b), "--gen-file", "data/zprime_narrow_gen.json", "--trigger-eff", "data/zprime_trigger_efficiencies.json"]
+        subprocess.call(args)
+
+
+
+
+
