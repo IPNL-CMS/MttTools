00fc08251c728c8d40dc7989c0a59d6361b72208
Sébastien Brochet <s.brochet@ipnl.in2p3.fr>
2014-05-06 13:55:41 +0200

    [Theta] Update model

--- Theta/README.md
+++ Theta/README.md
@@ -18,11 +18,11 @@ We have four observables, based on number of b-tagged jets and lepton flavor :
 ### MC
 
  - `TT`: TTbar powheg
- - `T[bar]_s`: single [anti-]top, s-channel
- - `T[bar]_t`: single [anti-]top, t-channel
- - `T[bar]_tW`: single [anti-]top, tW-channel
- - `Z*Jets`: Z + * jets (DY)
- - `W*Jets`: W + * jets
+ - `single_top`: single top
+ - `single_antitop`: single anti-top
+ - `dibosons`: di-bosons
+ - `zjets`: Z + * jets (DY)
+ - `wjets`: W + * jets
 
 ### Signal
  - `H*_scalar`: Massive Higgs of mass \*, scalar resonance
@@ -36,26 +36,49 @@ We have four observables, based on number of b-tagged jets and lepton flavor :
  - `lept`: lepton scale factor, +/- 1 sigma
  - `btag`: b-tagging scale factor, +/- 1 sigma
  - `pu`: PU reweighting, +/- 1 sigma
+ - `matching`: matching generator uncertainties
+ - `scale`: renormalization scale uncertainties
 
 # Normalization systematic uncertainties
 
-TODO
+ - `lumi`: 2.6%
+ - `TT`: 15%
+ - `single-top`: 30%
+ - `w jets / z jets`: 50%
+ - `dibosons`: 50%
 
 # How-to run
 
-## Pre-processing
+Possible model type:
+ - `scalar`: Higgs scalar
+ - `pseudoscalar`: Higgs pseudo-scalar
+ - `zprime_narrow`: Z' narrow
+ - `zprime_large`: Z' large 
+
+Possible options (all booleans):
+ - `stat_only`: run without any systematics
+ - `lumi_only`: run only with luminosity uncertainty
+ - `no_data`: run only expected, no observed
+ - `no_shape_syst`: run without any shape uncertainties, only normalisation uncertainties
+
+## Steps
 
 Save analysis summary:
 ```shell
-theta_driver preprocess --model 'model: type = scalar' --workdir summary_scalar --analysis summary file.root
+theta_driver preprocess --model 'model: type = pseudoscalar' --workdir summary_scalar --analysis summary file.root
 ```
 
 Perform a maximum likehood fit of all the parameters :
 ```shell
-theta_driver preprocess --model 'model: type = scalar' --workdir mle_scalar --analysis mle file.root
+theta_driver preprocess --model 'model: type = pseudoscalar' --workdir mle_scalar --analysis mle file.root
 ```
 
-Compute expected and observed limit. This step only create necessary files to launch crab
+Compute expected and observed limit.
 ```shell
-theta_driver preprocess --model 'model: type = scalar' --workdir bayesian_scalar --analysis 'bayesian: iterations = 160000' file.root
+theta_driver preprocess --model 'model: type = pseudoscalar' --workdir bayesian_scalar --analysis 'bayesian: iterations = 160000' file.root
+```
+
+Compute pvalues
+```shell
+theta_driver preprocess --model 'model: type = pseudoscalar' --workdir pvalues2_scalar --analysis pvalues2 file.root
 ```
--- Theta/model.py
+++ Theta/model.py
@@ -1,31 +1,49 @@
-systs = ['jec', 'jer', 'pu', 'lept']
+STAT_ONLY = False
+LUMI_ONLY = False
+NO_DATA = False
+NO_SHAPE_SYST = False
+
+def getSystematic(name):
+    p = name.split("__")
+    if len(p) > 2:
+        return p[2]
+    else:
+        return None
+
+def getProcess(name):
+    p = name.split("__")
+    return p[1]
+
+def default_filter(name):
+    global STAT_ONLY
+    global LUMI_ONLY
+    global NO_DATA
+    global NO_SHAPE_SYST
+
+    if (STAT_ONLY or LUMI_ONLY or NO_SHAPE_SYST) and getSystematic(name) is not None:
+        return False
+
+    if NO_DATA and "DATA" in name:
+        return False
+
+    # Ignore scale & matching uncertainties, except for tt
+    syst = getSystematic(name)
+    if syst is not None:
+        if 'scale' in syst or 'matching' in syst:
+            process = getProcess(name)
+            if 'TT' not in process:
+                return False
+
+    return True
+
 
 def select_scalar_signal_hist(name):
 
-    # Keep only jec syst
-    #if 'up' in name or 'down' in name:
-        #ret = False
-        #for syst in systs:
-            #ret = ret or syst in name
+    if not default_filter(name):
+        return False
 
-        #if not ret:
-            #return False
-
-    # Accept MC
-    #if '__T_tW' in name:
-        #return False
-
-    #if '__Tbar_' in name:
-        #return False
-
-    #if '__W' in name:
-        #return False
-
-    #if '__Z' in name:
-        #return False
-
-    if not "scalar" in name:
-        return True
+    if "zp" in name:
+        return False
 
     if '_pseudoscalar' in name:
         return False
@@ -34,58 +52,95 @@ def select_scalar_signal_hist(name):
 
 def select_pseudoscalar_signal_hist(name):
 
-    # Accept MC
-    if not "scalar" in name:
-        return True
+    if not default_filter(name):
+        return False
+
+    # Exclude Z'
+    if "zp" in name:
+        return False
 
     if '_scalar' in name:
         return False
 
+    #if "pseudoscalar" in name and (not '400' in name):
+        #return False
+
+    return True
+
+def select_zp_narrow_signal_hist(name):
+
+    if not default_filter(name):
+        return False
+
+    # Exclude Higgs
+    if "scalar" in name:
+        return False
+
+    if "large" in name:
+        return False
+
+    if 'zp' in name:
+        p = getProcess(name)
+        m = p.split('_')[0]
+        m = int(m[2:])
+
+        if m > 1000:
+            return False
+
     return True
 
 def build_theta_model(file, filter, signal):
+    global LUMI_ONLY
+    global STAT_ONLY
 
     model = build_model_from_rootfile(file, filter, include_mc_uncertainties = True)
     model.fill_histogram_zerobins()
     model.set_signal_processes(signal)
 
-    for p in model.processes:
-        model.add_lognormal_uncertainty('lumi', math.log(1.026), p)
+    if not STAT_ONLY:
+        for p in model.processes:
+            model.add_lognormal_uncertainty('lumi', math.log(1.026), p)
 
-    model.add_lognormal_uncertainty('ttbar_rate', math.log(1.50), 'TT')
+    if not STAT_ONLY and not LUMI_ONLY:
+        model.add_lognormal_uncertainty('ttbar_rate', math.log(1.15), 'TT')
 
-    singletop = ["T_t", "T_s", "T_tW"];
-    for p in singletop:
-        model.add_lognormal_uncertainty('st_rate', math.log(1.50), p)
+        model.add_lognormal_uncertainty('st_rate', math.log(1.30), 'single_top')
+        model.add_lognormal_uncertainty('sat_rate', math.log(1.30), 'single_antitop')
 
-    singletop = ["Tbar_t", "Tbar_s", "Tbar_tW"];
-    for p in singletop:
-        model.add_lognormal_uncertainty('st_rate', math.log(1.50), p)
+        model.add_lognormal_uncertainty('w_rate', math.log(1.50), 'wjets')
+        model.add_lognormal_uncertainty('z_rate', math.log(1.50), 'zjets')
 
-    Ws = ["W1Jets", "W2Jets", "W3Jets", "W4Jets"]
-    for W in Ws:
-        model.add_lognormal_uncertainty('w_rate', math.log(2.00), W)
-
-    Zs = ["Z1Jets", "Z2Jets", "Z3Jets", "Z4Jets"]
-    for Z in Zs:
-        model.add_lognormal_uncertainty('z_rate', math.log(2.00), Z)
+        model.add_lognormal_uncertainty('diboson_rate', math.log(1.50), 'dibosons')
 
     for p in model.distribution.get_parameters():
         d = model.distribution.get_distribution(p)
         if d['typ'] == 'gauss' and d['mean'] == 0.0 and d['width'] == 1.0:
-            model.distribution.set_distribution_parameters(p, range = [-5.0, 5.0])
+            model.distribution.set_distribution_parameters(p, range = [-30.0, 30.0])
 
     return model
 
-def build_model(type):
+def build_model(type, stat_only = False, lumi_only = False, no_data = False, no_shape_syst = False):
+    global STAT_ONLY
+    global LUMI_ONLY
+    global NO_DATA
+    global NO_SHAPE_SYST
+
+    STAT_ONLY = stat_only
+    LUMI_ONLY = lumi_only
+    NO_DATA = no_data
+    NO_SHAPE_SYST = no_shape_syst
 
     options = Options()
     options.set('global', 'debug', 'True')
 
     if type == "scalar":
-        return build_theta_model("higgs_analysis_lepton.root", select_scalar_signal_hist, "H*_scalar")
+        return build_theta_model("theta_histos_merged_BW10.root", select_scalar_signal_hist, "H*_scalar")
     elif type == "pseudoscalar":
-        return build_theta_model("higgs_analysis_lepton.root", select_pseudoscalar_signal_hist, "H*_pseudoscalar")
+        return build_theta_model("theta_histos_merged_BW10.root", select_pseudoscalar_signal_hist, "H*_pseudoscalar")
+    elif type == "zprime_narrow":
+        return build_theta_model("theta_histos_merged.root", select_zp_narrow_signal_hist, "zp*_narrow")
+    elif type == "zprime_large":
+        return build_theta_model("theta_histos_merged.root", select_zp_large_signal_hist, "zp*_large")
 
     return None
 
