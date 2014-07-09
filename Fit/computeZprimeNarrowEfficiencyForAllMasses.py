1de86c540a121782330c7efd39798c404894c475
Anne-Laure Pequegnot <a-l.pequegnot@ipnl.in2p3.fr>
2014-07-09 15:01:22 +0200

    Add efficiency for 500 and 2000 (copy from closest mass, error x2)

--- Fit/computeZprimeNarrowEfficiencyForAllMasses.py
+++ Fit/computeZprimeNarrowEfficiencyForAllMasses.py
@@ -7,7 +7,7 @@ btag = [1, 2]
 
 for mass in masses:
     for b in btag:
-        args = ["./computeEfficiencies", "-m", str(mass), "--b-tag", str(b), "--gen-file", "data/zprime_narrow_gen.json"]
+        args = ["./computeEfficiencies", "-m", str(mass), "--b-tag", str(b), "--gen-file", "data/zprime_narrow_gen.json", "--trigger-eff", "data/zprime_trigger_efficiencies.json"]
         subprocess.call(args)
 
 
--- Fit/computeZprimeNarrowEfficiencySystForAllMasses.py
+++ Fit/computeZprimeNarrowEfficiencySystForAllMasses.py
@@ -4,13 +4,13 @@ import subprocess
 
 masses = [500, 750, 1000, 1250, 1500, 2000]
 btag = [1, 2]
-systs = ["jec", "jer", "pu"]
+systs = ["jec", "jer", "pu", "btagsyst", "lept"]
 
 for mass in masses:
     for b in btag:
         for syst in systs:
             for sign in ["up", "down"]:
-                args = ["./computeEfficiencies", "-m", str(mass), "--b-tag", str(b), "--gen-file", "data/zprime_narrow_gen.json", "--%s" % syst, sign]
+                args = ["./computeEfficiencies", "-m", str(mass), "--b-tag", str(b), "--gen-file", "data/zprime_narrow_gen.json", "--trigger-eff", "data/zprime_trigger_efficiencies.json", "--%s" % syst, sign]
                 subprocess.call(args)
 
 
