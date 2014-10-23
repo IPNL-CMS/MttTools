36e6a2b82d43a8edd0c0c37e552676ecf748b972
Anne-Laure Pequegnot <a-l.pequegnot@ipnl.in2p3.fr>
2014-10-23 15:49:28 +0200

    Git mergetool is back! \o/

--- setup_lyoserv_sl6_env.sh
+++ setup_lyoserv_sl6_env.sh
@@ -10,5 +10,6 @@ source /cvmfs/cms.cern.ch/slc6_amd64_gcc472/external/gdb/7.6-cms/etc/profile.d/i
 source /cvmfs/cms.cern.ch/slc6_amd64_gcc472/external/python/2.6.4/etc/profile.d/init.sh
 source /cvmfs/cms.cern.ch/slc6_amd64_gcc472/external/boost/1.51.0-cms5/etc/profile.d/init.sh
 source /cvmfs/cms.cern.ch/slc6_amd64_gcc472/external/git/1.8.3.1-cms2/etc/profile.d/init.sh
+export GIT_EXEC_PATH=/cvmfs/cms.cern.ch/slc6_amd64_gcc472/external/git/1.8.3.1-cms2/libexec/git-core
 
 export BOOST_ROOT
