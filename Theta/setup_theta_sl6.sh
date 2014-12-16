8a81885c3b4ca0b87953426f77e1f08ab1649632
Anne-Laure Pequegnot <a-l.pequegnot@ipnl.in2p3.fr>
2014-12-16 15:28:14 +0100

    Git available again

--- Theta/setup_theta_sl6.sh
+++ Theta/setup_theta_sl6.sh
@@ -1,1 +1,3 @@
 source /gridgroup/cms/brochet/HTT/theta/setup_lyoserv_env_sl6.sh
+source /cvmfs/cms.cern.ch/slc6_amd64_gcc472/external/git/1.8.3.1-cms2/etc/profile.d/init.sh
+export GIT_EXEC_PATH=/cvmfs/cms.cern.ch/slc6_amd64_gcc472/external/git/1.8.3.1-cms2/libexec/git-core
